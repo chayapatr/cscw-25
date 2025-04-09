1c292a007f6c6bedb0eb97300da0bc6417228865
chayapatr <chayapatr.arc@gmail.com>
2025-04-09 16:43:07 -0400

    feat: things

--- functions/triplets.py
+++ functions/triplets.py
@@ -13,11 +13,18 @@ def parse_key_list(key):
         # keys = [f"{t}", f"{t}>{f}"]
         keys = [ f"{t}", f"{t}>{f.split(":")[0]}"] #, f"{t}>{f.split(":")[0]}"]
     elif(not f):
+        if(s == "user" or s == "system"):
+            keys = [f"{t}"]
         # keys = [f"{t}", f"{t}|{s}"]
-        keys = [f"{t}", f"{t}|{s.split(":")[0]}"]
+        else:
+            keys = [f"{t}", f"{t}|{s.split(":")[0]}"]
     else:
+        if(s == "user" or s == "system"):
+            # print("FOO!", key, [f"{t}", f"{t}>{f.split(":")[0]}"])
+            keys = [f"{t}", f"{t}>{f.split(":")[0]}"]
         # keys= [f"{t}", f"{t}|{s}", f"{t}|{s}>{f}"]
-        keys= [f"{t}", f"{t}|{s.split(":")[0]}"] #, f"{t}|{s.split(":")[0]}>{f.split(":")[0]}"]
+        else:
+            keys= [f"{t}", f"{t}|{s.split(":")[0]}"] #, f"{t}|{s.split(":")[0]}>{f.split(":")[0]}"]
     
     return [ k.replace("-", "_").replace(" ", "_") for k in keys ]
 
@@ -31,8 +38,12 @@ def parse_key(key, full=False):
     elif(not s):
         return f"{t}>{f}".replace("-", "_").replace(" ", "_") if full else f"{t}>{f.split(":")[0]}".replace("-", "_").replace(" ", "_")
     elif(not f):
+        if(s == "user" or s == "system"):
+            return f"{t}".replace("-", "_").replace(" ", "_") if full else f"{t}".replace("-", "_").replace(" ", "_")
         return f"{t}|{s}".replace("-", "_").replace(" ", "_") if full else f"{t}|{s.split(":")[0]}".replace("-", "_").replace(" ", "_")
     else:
+        if(s == "user" or s == "system"):
+            return f"{t}>{f}".replace("-", "_").replace(" ", "_") if full else f"{t}>{f.split(":")[0]}".replace("-", "_").replace(" ", "_")
         return f"{t}|{s}>{f}".replace("-", "_").replace(" ", "_") if full else f"{t}|{s.split(":")[0]}".replace("-", "_").replace(" ", "_")
 
 def parse_subject(s):
